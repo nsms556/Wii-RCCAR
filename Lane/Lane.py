3ff92a63e41a37c70889d36657b7f781f8add3f3
nsms556 <nsms556@kakao.com>
2020-05-26 17:18:37 +0900

    commit master

--- Lane/Lane.py
+++ Lane/Lane.py
@@ -2,33 +2,6 @@ import cv2
 import numpy as np
 import math
 
-def gstreamer_pipeline(
-    capture_width=640,
-    capture_height=360,
-    display_width=640,
-    display_height=360,
-    framerate=30,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
-
 def convert_to_HSV(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     return hsv
@@ -200,14 +173,3 @@ def laneTracking(capture) :
         key = cv2.waitKey(1)
         if key == 27:
             break
-
-if __name__ == '__main__' :
-    video = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
-
-    if video.isOpened() :
-        laneTracking(video)
-    else :
-        print('NoVideo')
-
-    video.release()
-    cv2.destroyAllWindows()
